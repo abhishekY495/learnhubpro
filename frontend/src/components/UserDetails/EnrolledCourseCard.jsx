b4a1ec7bad3ccd291b310b1567c00225f1033614
Abhishek <abhishekY495.sec@gmail.com>
2024-01-20 16:02:34 +0530

    Styling: progess percent

--- frontend/src/components/UserDetails/EnrolledCourseCard.jsx
+++ frontend/src/components/UserDetails/EnrolledCourseCard.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { calculatePercentage } from "../../utils/calculatePercentage";
+
 export const EnrolledCourseCard = ({ course }) => {
+  const percent = calculatePercentage(course?.content);
+
   return (
     <Link
       to={`/enrolledcourse/${course?._id}`}
-      className="bg-neutral-200 hover:bg-neutral-300 p-3 rounded-md"
+      className="bg-neutral-200 hover:bg-neutral-300 p-2 rounded-md"
       title={course?.title}
     >
-      <img
-        src={course?.thumbnail}
-        alt={course?._title}
-        className="w-full object-cover rounded-md"
-      />
-      <p className="line-clamp-1 font-semibold mt-1 text-lg">{course?.title}</p>
+      <div className="relative">
+        <img
+          src={course?.thumbnail}
+          alt={course?._title}
+          className="w-full object-cover rounded-md"
+        />
+        <p className="absolute bg-green-500 top-0 right-0 px-2 py-[2xp] font-semibold rounded-bl-sm">
+          {percent}%
+        </p>
+      </div>
+      <p className="line-clamp-1 font-semibold mt-1 pl-[2px] text-lg">
+        {course?.title}
+      </p>
     </Link>
   );
 };
